9866bd7add8ca2458ad116351d8318b00a8f3af1
vijay-devops123 <besthavijay9523@gmail.com>
2026-02-09 05:36:41 +0000

    we have completed addition program for calculator

---
+++ Calculator/addition.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Read two numbers
+echo "Enter first number:"
+read a
+
+echo "Enter second number:"
+read b
+
+# Perform addition
+sum=$((a + b))
+
+# Display result
+echo "Sum is: $sum"
